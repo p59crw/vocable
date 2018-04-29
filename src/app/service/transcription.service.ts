af44915aa8f22e4b5f3dca6c7a5cd97e3a22fa16
c_white87 <curtis.white@wekaplex.com>
2018-04-29 14:30:47 -0230

    Added permutate() method

--- src/app/service/transcription.service.ts
+++ src/app/service/transcription.service.ts
@@ -5,10 +5,20 @@ import { InventoryService } from './inventory.service';
 @Injectable()
 export class TranscriptionService {
 
+  // Variables
   wordStructureInput: HTMLInputElement;
 
+  // Constructor
   constructor(private inventoryService: InventoryService) { }
 
+  /**
+  * Reads input from wordStructureInput element, removes duplicates, and creates
+  * arrays which are sent to prepareComponents() where sounds are assigned to
+  * characters.
+  * @param wordStructure    array of word structure format with parentheses kept
+  * @param uniqueChars      array of wordStructure characters with duplicates removed
+  * @returns                array of permutations
+  */
   private parseWordStructure(): Array<string> {
     this.wordStructureInput = <HTMLInputElement>document.getElementById('word_structure');
     const wordStructure = [];
@@ -41,6 +51,14 @@ export class TranscriptionService {
     return this.prepareComponents(wordStructure, uniqueChars, this.inventoryService.getInventory());
   }
 
+  /**
+  * Assigns sounds to a letter, such as consonants to 'C'. Then these assignments
+  * are sent to generateWords() to be permutated.
+  * @param wordStructure    array of word structure format with parentheses kept
+  * @param uniqueChars      array of wordStructure characters with duplicates removed
+  * @param inventory        array of all sounds selected in inventory
+  * @returns                array of permutations
+  */
   private prepareComponents(wordStructure: Array<string>, uniqueChars: Array<string>, inventory: Array<Sound>): Array<string> {
     const wordStructureComponents = new Array();
     const assignSoundsToLetter = function(letter: string, sounds: Array<Sound>) {
@@ -71,14 +89,34 @@ export class TranscriptionService {
     return this.generateWords(wordStructure, wordStructureComponents);
   }
 
-  private generateWords(structure: Array<string>, components: Array<string>) {
+  /**
+  * Accepts structure array and array of components. Iterates over components
+  * and generates permutations by replacing structure with Sound components.
+  * @param structure    array of word structure format
+  * @param components   array of sounds assigned to a structural element
+  * @returns            array of permutations
+  */
+  private generateWords(structure: Array<string>, components: Array<Sound>) {
     const glosses = new Array();
-    const permutate = function(array) {
-
+    let string;
+    const permutate = function(elements) {
+      for (let i = 0; i < elements[1].length; i++) {
+        string += elements[1][i].ipa_unicode;
+      }
+      return string;
     };
-    return null;
+    for (let i = 0; i < components.length; i++) {
+      glosses[i] = permutate(components[i]);
+    }
+    console.log(glosses);
+    return glosses;
   }
 
+  /**
+  * Called when submit button is clicked. Started permutation process and
+  * returns permutations to be shown on page.
+  * @returns  array of permutations
+  */
   public getGlosses(): Array<string> {
     return this.parseWordStructure();
   }
