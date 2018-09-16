fe978be201354a969e364770f72c226e69cca418
c_white87 <curtis.white@wekaplex.com>
2018-09-16 01:29:21 -0230

    Updated layout with animated sidebar navigation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,18 @@ export class AppComponent implements OnInit {
 
   constructor(private loaderService: LoaderService) { }
 
+  /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+  openNav() {
+    document.getElementById('sidenav-wrapper').style.width = '250px';
+    document.getElementById('page-content-wrapper').style.marginLeft = '250px';
+  }
+
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+  closeNav() {
+    document.getElementById('sidenav-wrapper').style.width = '0';
+    document.getElementById('page-content-wrapper').style.marginLeft = '0';
+  }
+
   ngOnInit() {
     this.loaderService.status.subscribe((val: boolean) => {
       this.showLoader = val;
