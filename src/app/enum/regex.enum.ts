9e037b92629c41840124e5fdc1a3d55353f2c5c2
c_white87 <curtis.white@wekaplex.com>
2018-09-30 18:47:39 -0230

    Updated Regex enum

--- src/app/enum/regex.enum.ts
+++ src/app/enum/regex.enum.ts
@@ -1,4 +1,5 @@
 export enum Regex {
   WORD_STRUCTURE_RULE = '([(][A-Z|a-z|0-9|@|!|$|&]{1,}[)]|[A-Z|a-z|0-9|@|!|$|&]{1,})',
-  TRANSCRIPTION_RULE = '(?<=^|;)(\\/?_?[a-zA-Z0-9]*<(?:[a-zA-Z]|C[uv]?|K[g]|L[g]|Z[g]|X[g]|V[n])>[a-zA-Z0-9]*_?\\/?):([a-zA-Z0-9]+|\\*)(?=;|$)'
+  TRANSCRIPTION_RULE = '(?<=^|;)(\\/?_?[a-zA-Z0-9]*<(?:[a-zA-Z]|C[uv]?|K[g]|L[g]|Z[g]|X[g]|V[n])>[a-zA-Z0-9]*_?\\/?):([a-zA-Z0-9]+|\\*)(?=;|$)',
+  NUMERIC = '^[0-9]+$'
 }
