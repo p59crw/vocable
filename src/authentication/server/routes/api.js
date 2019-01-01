0dbfcf4d5d5e4f962e3be3abf01a5b3ce07cb313
Curtis White <c-white@nf.sympatico.ca>
2019-01-01 14:51:33 -0330

    Updated login API

--- src/authentication/server/routes/api.js
+++ src/authentication/server/routes/api.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/user')
 const mongoose = require('mongoose')
 const db = "mongodb://admin:Pixel8r0!@ds145704.mlab.com:45704/vocable_db"
 
@@ -13,6 +14,38 @@ mongoose.connect(db, err => {
 
 router.get('/', (req, res) => {
     res.send('From API Route')
+})
+
+router.post('/register', (req, res) => {
+    let userData = req.body
+    let user = new User(userData)
+    user.save((err, newUser) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.status(200).send(newUser)
+        }
+    })
+})
+
+router.post('/login', (req, res) => {
+    let userData = req.body
+
+    User.findOne({email: userData.email}, (err, user) => {
+        if (err) {
+            console.log(err)
+        } else {
+            if (!user) {
+                res.status(401).send('Invalid Email')
+            } else {
+                if (user.password !== userData.password) {
+                    res.status(401).send('Incorrect Password')
+                } else {
+                    res.status(200).send(user)
+                }
+            }
+        }
+    })
 })
 
 module.exports = router
