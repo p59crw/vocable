bb41d3126ed8e687340062ad5df1c732a0467870
Curtis White <c-white@nf.sympatico.ca>
2019-02-16 22:21:35 -0330

    Revert "Added authentication check"
    This reverts commit e87f06e06b38d71fbc4400edd6aa4676cab4296c.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthenticationService, LoaderService } from './service';
+import { LoaderService } from './service/loader.service';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +9,7 @@ import { AuthenticationService, LoaderService } from './service';
 export class AppComponent implements OnInit {
   showLoader: boolean;
 
-  constructor(private auth: AuthenticationService, private loaderService: LoaderService) { }
+  constructor(private loaderService: LoaderService) { }
 
   /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
   openNav() {
@@ -21,15 +21,6 @@ export class AppComponent implements OnInit {
   closeNav() {
     document.getElementById('sidenav-wrapper').style.width = '0';
     document.getElementById('page-content-wrapper').style.marginLeft = '0';
-  }
-
-  /* Check whether user is currently logged in */
-  isAuthenticated() {
-    if (localStorage.key(0)) {
-      return true;
-    } else {
-      return false;
-    }
   }
 
   ngOnInit() {
